2c9636a1eae8bb6fb15e855a66c0d3128ddfec39
Melqui <43079783+melquisedeque-magalhaes@users.noreply.github.com>
2020-12-24 14:44:31 -0300

    Updates

---
+++ precache-manifest.51c99a6c43691c6560cf67f1675f9c0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735907da6d5d5fa5697724e73ae8b353",
+    "url": "/gobarber-web/index.html"
+  },
+  {
+    "revision": "c1924a9cedaec806af53",
+    "url": "/gobarber-web/static/js/2.42e48ed5.chunk.js"
+  },
+  {
+    "revision": "0e6aac4919f09d5c099b512c4cd9aa09",
+    "url": "/gobarber-web/static/js/2.42e48ed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27375743b2b21c1ed1e3",
+    "url": "/gobarber-web/static/js/main.f248c63b.chunk.js"
+  },
+  {
+    "revision": "cc8eb60cf63871836a9c",
+    "url": "/gobarber-web/static/js/runtime-main.0083ccb1.js"
+  },
+  {
+    "revision": "a49b07ec538713cf0e3910693544c7be",
+    "url": "/gobarber-web/static/media/logo.a49b07ec.svg"
+  },
+  {
+    "revision": "6eac314f78fe224c4d769fb646523773",
+    "url": "/gobarber-web/static/media/sign-in-background.6eac314f.png"
+  },
+  {
+    "revision": "c4d8f988b9c6499d56702a398b2f3d8b",
+    "url": "/gobarber-web/static/media/sign-up-background.c4d8f988.png"
+  }
+]);
